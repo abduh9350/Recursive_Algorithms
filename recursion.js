9bbf5052eded0aee639b090cedc1b9237fe048ae
Abdu Jemal <abduh9350@gmail.com>
2020-08-19 04:17:12 -0400

    questions and answers 1 to 7

--- recursion.js
+++ recursion.js
@@ -1,6 +1,8 @@
 /* 
 1. Counting Sheep
-Write a recursive function that counts how many sheep jump over the fence. Your program should take a number as input. That number should be the number of sheep you have. The function should display the number along with the message "Another sheep jumps over the fence" until no more sheep are left.
+Write a recursive function that counts how many sheep jump over the fence. Your program should take a number as input.
+ That number should be the number of sheep you have. 
+ The function should display the number along with the message "Another sheep jumps over the fence" until no more sheep are left.
 
 Input: 3
 Output:
@@ -21,7 +23,9 @@ console.log(countingSheep(3));
 
 /*
 2. Power Calculator
-Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers)
+Write a function called powerCalculator() that takes two parameters, an integer as a base, and 
+another integer as an exponent. The function returns the value of the base raised to the power of the exponent. 
+Use only exponents greater than or equal to 0 (positive numbers)
 
 powerCalculator(10,2) should return 100
 powerCalculator(10,-2) should return exponent should be >= 0
@@ -40,7 +44,10 @@ function powerCalculator(base, exp) {
 
 console.log(powerCalculator(20, -2));
 
-//3
+/*
+3. Reverse String
+Write a function that reverses a string. Take a string as input, reverse the string, and return the new string.
+*/
 function reverseString(str) {
   if(str.length === 1) {
     return str;
@@ -51,7 +58,13 @@ function reverseString(str) {
 
 console.log(reverseString('abdu hussen jemal'));
 
-//4
+/*
+4. nth Triangular Number
+Calculate the nth triangular number. A triangular number counts the objects that can form an equilateral triangle.
+ The nth triangular number is the number of dots composing a triangle with n dots on a side, 
+ and is equal to the sum of the n natural numbers from 1 to n. 
+ This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
+*/
 function triangleNumber(num) {
   if(num === 1) {
     return 1;
@@ -62,7 +75,14 @@ function triangleNumber(num) {
 
 console.log(triangleNumber(9));
 
-//5
+/*
+5. String Splitter
+Write a recursive function that splits a string based on a separator (similar to String.prototype.split).
+Don't use JS array's split function to solve this problem.
+
+Input: 02/20/2020
+Output: ["02", "20", "2020"]
+*/
 function stringSplitter(string,separator){
   if(string.length === 1 && string[0] !== separator) {
     return string[0];
@@ -78,7 +98,13 @@ function stringSplitter(string,separator){
 
 console.log(stringSplitter('02/20/2020', '/'));
 
-//6
+/*
+6. Fibonacci
+Write a recursive function that prints the Fibonacci sequence of a given number. 
+The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. 
+For example, the 7th Fibonacci number in a Fibonacci sequence is 13. 
+The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+*/
 function fibo(num) {
   if(num === 1) {
     return 1;
@@ -103,6 +129,12 @@ function fibonacci(num) {
 
 fibonacci(7);
 
+/*
+7. Factorial
+Write a recursive function that finds the factorial of a given number. 
+The factorial of a number can be found by multiplying that number by each number between itself and 1. 
+For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+*/
 function factorial(num) {
   if(num === 1) {
     return 1;
